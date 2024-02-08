a3c7e6d301a46379c12596518a42cfb998f4b61c
hanglma <martin.hangl@jhg-traunreut.de>
2024-02-08 22:05:55 +0100

    add: dinoGame

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,4 @@
-// import "./style.css";
-// import typescriptLogo from "./pictures/typescript.svg";
-// import viteLogo from "./pictures/vite.svg";
-
-// document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-//   <div>
-//     <a href="https://vitejs.dev" target="_blank">
-//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://www.typescriptlang.org/" target="_blank">
-//       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-//     </a>
-//     <h1>Vite + TypeScript</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite and TypeScript logos to learn more
-//     </p>
-//   </div>
-// `;
-
+// Orientations Sensoren auslesen
 function orientationAuslesen(ereignis: DeviceOrientationEvent): void {
   const winkelAlpha: number = Math.round(ereignis.alpha! * 1000) / 1000;
   const winkelBeta: number = Math.round(ereignis.beta! * 1000) / 1000;
@@ -31,7 +10,7 @@ function orientationAuslesen(ereignis: DeviceOrientationEvent): void {
   document.getElementById("kompass")!.style.transform =
     "rotate(" + winkelAlpha + "deg)";
 }
-
+// bewegungs-Sensoren auslesen
 function bewegungAuslesen(ereignis: DeviceMotionEvent): void {
   const beschleunigungX: number =
     Math.round(ereignis.accelerationIncludingGravity!.x! * 1000) / 1000;
@@ -45,21 +24,59 @@ function bewegungAuslesen(ereignis: DeviceMotionEvent): void {
   document.getElementById("accelZ")!.innerHTML = beschleunigungZ!.toString();
 }
 
+// aufruf der Methode für Auslesung der Sensoren
 window.onload = function () {
   window.addEventListener("deviceorientation", orientationAuslesen, false);
   window.addEventListener("devicemotion", bewegungAuslesen, false);
-  window.addEventListener("devicemotion", sprungDetection, false);
+  // window.addEventListener("devicemotion", jump, false);
+  window.addEventListener("keypress", jumpSpace);
 };
 
-function sprungDetection(ereignis: DeviceMotionEvent): boolean {
+//* Sensor Dino Game :)
+
+let character = document.getElementById("character");
+let block = document.getElementById("block");
+
+/* function jump(ereignis: DeviceMotionEvent): void {
   // lesen von z-Beschleunigung
   const beschleunigungZ: number = ereignis.accelerationIncludingGravity!.z!;
   // falls z-B größer 12 so wird ein Sprung aktiviert => return true
   if (beschleunigungZ > 12) {
-    document.getElementById("jump")!.innerHTML = "&#x1F534";
-    return true;
+    character?.classList.add("animate");
+    console.log("jump");
   } else {
     document.getElementById("jump")!.innerHTML = "&#x1F7E2";
-    return false;
+  }
+} */
+
+function jumpSpace(key: any) {
+  if ((key = "Space")) {
+    if (character?.classList.contains("animate") == false) {
+      character!.classList.add("animate");
+      setTimeout(function () {
+        character!.classList.remove("animate");
+      }, 550);
+      console.log(character?.classList);
+    }
+    if (isDead == true) {
+      window.location.reload();
+    }
   }
 }
+
+let isDead: boolean = false;
+
+let checkDead = setInterval(function () {
+  let characterTop = parseInt(
+    window.getComputedStyle(character as Element).getPropertyValue("top")
+  );
+  let blockLeft = parseInt(
+    window.getComputedStyle(block as Element).getPropertyValue("left")
+  );
+  if (blockLeft > 40 && blockLeft < 60 && characterTop >= 130) {
+    isDead = true;
+    block?.setAttribute("style", "animation: none");
+    block?.setAttribute("style", "display: none");
+    document.getElementById("lose")!.innerHTML = "YOU LOSE :(";
+  }
+}, 10);
